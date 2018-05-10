e799ed837baa7013806ebde8507461749d8b3244
Dingo <Dingo@DESKTOP-NGC3LR6>
2018-05-10 13:32:34 +0200

    Add title Add boostrap link Add responsive design scaling

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  
+        <title>Vestas Carpool</title>
     </head>
     <body>
         <?php
