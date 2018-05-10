46cd46d8069e6ad8ca1acd5547ac68ace8c9808e
Dingo <Dingo@DESKTOP-NGC3LR6>
2018-05-10 13:51:44 +0200

    Add button

--- index.php
+++ index.php
@@ -7,12 +7,15 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
         <title>Vestas Carpool</title>
     </head>
     <body>
+        <div class="container-fluid">
+            <button type="button" class="btn btn-default">Create Ride</button>
+        </div>
         <?php
         // put your code here
         ?>
